1ca19652b4e9b1d0bdb40bb650111d40cb5a76c4
kasia251011 <katarzyna.jankowska000728@gmail.com>
2023-12-08 11:13:43 +0100

    Add localstorage for register status

--- src/controller/groups/addGroups.ts
+++ src/controller/groups/addGroups.ts
@@ -1,10 +1,14 @@
+import { REGISTER_STATE_KEY } from "../..";
 import GroupSchema, { Group } from "../../model/Group";
 import { Request, Response } from "express";
+import { RegisterStatus } from "../../model/common";
+import { localStorage } from "../..";
 
 export const addGroups = async (
   req: Request<{}, {}, Group[]>,
   res: Response
 ) => {
+  localStorage.setItem(REGISTER_STATE_KEY, RegisterStatus.USERS_REGISTER);
   return GroupSchema.insertMany<Group>(req.body)
     .then((groups) => {
       res.status(201).json(groups);
